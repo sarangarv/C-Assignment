844ebec396df4124e4d372f30980b6af10837826
sarangarv <sarangarv@gmail.com>
2025-10-24 21:03:43 +0530

    Add main structure

---
+++ logistics.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main() {
+
+    printf("\n=== Logistics Management System ===\n");
+        printf("1) City Management\n");
+        printf("2) Distance Management\n");
+        printf("3) Vehicle Info / Fuel Price\n");
+        printf("4) Create Delivery Request\n");
+        printf("5) List Deliveries\n");
+        printf("6) Delete Delivery\n");
+        printf("7) Find least-distance route (Query)\n");
+        printf("8) Reports\n");
+        printf("9) Save data now\n");
+        printf("10) Save & Exit\n");
+
+return 0;
+}
